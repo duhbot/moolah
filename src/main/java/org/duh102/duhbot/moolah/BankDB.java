66487b3ac784f70bd120c50f6523eb1ef1b56012
Will Morrow <duh102@gmail.com>
2018-08-30 16:02:30 -0400

    Remove fractional part of account

--- src/main/java/org/duh102/duhbot/moolah/BankDB.java
+++ src/main/java/org/duh102/duhbot/moolah/BankDB.java
@@ -26,7 +26,12 @@ public class BankDB {
       try
       {
         Statement stat = conn.createStatement();
-        stat.executeUpdate("CREATE TABLE IF NOT EXISTS bankaccount (user TEXT, channel TEXT, server TEXT, balance INTEGER, balancefractional INTEGER, lastmined INTEGER);");
+        //This table is not safe for networks with no nickserv or usage on several
+        //  networks (with different nickserv databases)! We only store the registered nick
+        stat.executeUpdate("CREATE TABLE IF NOT EXISTS bankAccount (uid INTEGER PRIMARY KEY, user TEXT UNIQUE NOT NULL, balance INTEGER NOT NULL DEFAULT 0, lastMined INTEGER NOT NULL DEFAULT (strftime('%s', datetime('now', '-1 day'))) );");
+        stat.executeUpdate("CREATE TABLE IF NOT EXISTS slotOutcome (outcomeid INTEGER PRIMARY KEY, uid INTEGER REFERENCES bankAccount(uid) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL, slotImages TEXT NOT NULL, wager INTEGER NOT NULL, payout INTEGER NOT NULL, payoutMul REAL NOT NULL, timestamp INTEGER NOT NULL DEFAULT (strftime('%s', CURRENT_TIMESTAMP)) );");
+        stat.executeUpdate("CREATE TABLE IF NOT EXISTS hiLoOutcome (outcomeid INTEGER PRIMARY KEY, uid INTEGER REFERNECES bankAccount(uid) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL, resultInt INTEGER NOT NULL, hiLo TEXT NOT NULL, wager INTEGER NOT NULL, payout INTEGER NOT NULL, payoutMul REAL NOT NULL, timestamp INTEGER NOT NULL DEFAULT (strftime('%s', CURRENT_TIMESTAMP)) );");
+        stat.executeUpdate("CREATE TABLE IF NOT EXISTS mineOutcome (outcomeid INTEGER PRIMARY KEY, uid INTEGER REFERENCES bankAccount(uid) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL, mineFractions INTEGER NOT NULL, richness REAL NOT NULL, yield INTEGER NOT NULL, timestamp INTEGER NOT NULL DEFAULT (strftime('%s', CURRENT_TIMESTAMP)) );");
         conn.close();
       }
       catch(java.sql.SQLException sqle)
--- src/test/java/org/duh102/duhbot/moolah/BankAccountTest.java
+++ src/test/java/org/duh102/duhbot/moolah/BankAccountTest.java
@@ -13,26 +13,13 @@ class BankAccountTest {
    */
   @Test
   public void testConstructorEasy() throws Exception {
-    BankAccount a = new BankAccount("user", 100l, (byte)26);
+    BankAccount a = new BankAccount("user", 100l);
     assertEquals(a.balance, 100l);
-    assertEquals(a.fracbalance, (byte)26);
-  }
-  @Test
-  public void testConstructorNegativeFrac() throws Exception {
-    assertThrows(ImproperFractionalAmount.class, () -> {
-        BankAccount a = new BankAccount("user", 100l, (byte)-26);
-    });
-  }
-  @Test
-  public void testConstructorBigFrac() throws Exception {
-    assertThrows(ImproperFractionalAmount.class, () -> {
-        BankAccount a = new BankAccount("user", 100l, (byte)101);
-    });
   }
   @Test
   public void testConstructorNegativeBal() throws Exception {
     assertThrows(ImproperBalanceAmount.class, () -> {
-        BankAccount a = new BankAccount("user", -100l, (byte)26);
+        BankAccount a = new BankAccount("user", -100l);
     });
   }
 
@@ -41,89 +28,23 @@ class BankAccountTest {
    */
   @Test
   public void testAddEasy() throws Exception {
-    BankAccount a = new BankAccount("user", 0l, (byte)0);
-    a.addFunds(100l, (byte)0);
+    BankAccount a = new BankAccount("user", 0l);
+    a.addFunds(100l);
     assertEquals(a.balance, 100l);
-    assertEquals(a.fracbalance, (byte)0);
-  }
-
-  @Test
-  public void testAddFractionalNoCarry() throws Exception {
-    BankAccount a = new BankAccount("user", 0l, (byte)50);
-    a.addFunds(0l, (byte)49);
-    assertEquals(a.balance, 0l);
-    assertEquals(a.fracbalance, (byte)99);
-  }
-
-  @Test
-  public void testAddFractionalCarryZero() throws Exception {
-    BankAccount a = new BankAccount("user", 0l, (byte)50);
-    a.addFunds(0l, (byte)50);
-    assertEquals(a.balance, 1l);
-    assertEquals(a.fracbalance, (byte)0);
-  }
-
-  @Test
-  public void testAddFractionalCarryOne() throws Exception {
-    BankAccount a = new BankAccount("user", 0l, (byte)50);
-    a.addFunds(0l, (byte)51);
-    assertEquals(a.balance, 1l);
-    assertEquals(a.fracbalance, (byte)1);
   }
 
   @Test
   public void testAddTypeCastBal() throws Exception {
-    BankAccount a = new BankAccount("user", 0l, (byte)0);
-    a.addFunds(100, (byte)0);
+    BankAccount a = new BankAccount("user", 0l);
+    a.addFunds(100);
     assertEquals(a.balance, 100l);
-    assertEquals(a.fracbalance, (byte)0);
-  }
-
-  @Test
-  public void testAddTypeCastFrac() throws Exception {
-    BankAccount a = new BankAccount("user", 0l, (byte)0);
-    a.addFunds(100l, 0);
-    assertEquals(a.balance, 100l);
-    assertEquals(a.fracbalance, (byte)0);
-  }
-
-  @Test
-  public void testAddTypeCastBoth() throws Exception {
-    BankAccount a = new BankAccount("user", 0l, (byte)0);
-    a.addFunds(100, 0);
-    assertEquals(a.balance, 100l);
-    assertEquals(a.fracbalance, (byte)0);
-  }
-
-  @Test
-  public void testAddNegativeFrac() throws Exception {
-    BankAccount a = new BankAccount("User", 0l, (byte)0);
-    assertThrows(ImproperFractionalAmount.class, () -> {
-        a.addFunds(0l, (byte)-1);
-    });
-  }
-
-  @Test
-  public void testAddBigFrac() throws Exception {
-    BankAccount a = new BankAccount("User", 0l, (byte)0);
-    assertThrows(ImproperFractionalAmount.class, () -> {
-        a.addFunds(0l, (byte)101);
-    });
-  }
-
-  @Test
-  public void testAddBigCastFrac() throws Exception {
-    BankAccount a = new BankAccount("User", 0l, (byte)0);
-    assertThrows(ImproperFractionalAmount.class, () -> {
-        a.addFunds(0l, 200);
-    });
   }
 
   @Test
   public void testAddNegativeBal() throws Exception {
-    BankAccount a = new BankAccount("User", 0l, (byte)0);
+    BankAccount a = new BankAccount("User", 0l);
     assertThrows(ImproperBalanceAmount.class, () -> {
-        a.addFunds(-1l, (byte)1);
+        a.addFunds(-1l);
     });
   }
 
@@ -133,89 +54,31 @@ class BankAccountTest {
 
   @Test
   public void testSubEasy() throws Exception {
-    BankAccount a = new BankAccount("user", 100l, (byte)0);
-    a.subFunds(50l, (byte)0);
+    BankAccount a = new BankAccount("user", 100l);
+    a.subFunds(50l);
     assertEquals(a.balance, 50l);
-    assertEquals(a.fracbalance, (byte)0);
   }
 
   @Test
   public void testSubInsufficientEasy() throws Exception {
-    BankAccount a = new BankAccount("user", 50l, (byte)0);
+    BankAccount a = new BankAccount("user", 50l);
     assertThrows(InsufficientFundsException.class, () -> {
-        a.subFunds(51l, (byte)0);
+        a.subFunds(51l);
     });
-  }
-
-  @Test
-  public void testSubInsufficientCarry() throws Exception {
-    BankAccount a = new BankAccount("user", 50l, (byte)0);
-    assertThrows(InsufficientFundsException.class, () -> {
-        a.subFunds(50l, (byte)1);
-    });
-  }
-
-  @Test
-  public void testSubCarry() throws Exception {
-    BankAccount a = new BankAccount("user", 51l, (byte)0);
-    a.subFunds(50l, (byte)1);
-    assertEquals(a.balance, 0l);
-    assertEquals(a.fracbalance, (byte)99);
   }
 
   @Test
   public void testSubTypeCastBal() throws Exception {
-    BankAccount a = new BankAccount("user", 100l, (byte)0);
-    a.subFunds(50, (byte)10);
-    assertEquals(a.balance, 49l);
-    assertEquals(a.fracbalance, (byte)90);
-  }
-
-  @Test
-  public void testSubTypeCastFrac() throws Exception {
-    BankAccount a = new BankAccount("user", 100l, (byte)0);
-    a.subFunds(50l, 10);
-    assertEquals(a.balance, 49l);
-    assertEquals(a.fracbalance, (byte)90);
-  }
-
-  @Test
-  public void testSubTypeCastBoth() throws Exception {
-    BankAccount a = new BankAccount("user", 100l, (byte)0);
-    a.subFunds(50, 10);
-    assertEquals(a.balance, 49l);
-    assertEquals(a.fracbalance, (byte)90);
-  }
-
-  @Test
-  public void testSubNegativeFrac() throws Exception {
-    BankAccount a = new BankAccount("User", 1000l, (byte)0);
-    assertThrows(ImproperFractionalAmount.class, () -> {
-        a.subFunds(0l, (byte)-1);
-    });
-  }
-
-  @Test
-  public void testSubBigFrac() throws Exception {
-    BankAccount a = new BankAccount("User", 1000l, (byte)0);
-    assertThrows(ImproperFractionalAmount.class, () -> {
-        a.subFunds(0l, (byte)101);
-    });
-  }
-
-  @Test
-  public void testSubBigCastFrac() throws Exception {
-    BankAccount a = new BankAccount("User", 1000l, (byte)0);
-    assertThrows(ImproperFractionalAmount.class, () -> {
-        a.subFunds(0l, 200);
-    });
+    BankAccount a = new BankAccount("user", 100l);
+    a.subFunds(50);
+    assertEquals(a.balance, 50l);
   }
 
   @Test
   public void testSubNegativeBal() throws Exception {
-    BankAccount a = new BankAccount("User", 1000l, (byte)0);
+    BankAccount a = new BankAccount("User", 1000l);
     assertThrows(ImproperBalanceAmount.class, () -> {
-        a.subFunds(-1l, (byte)1);
+        a.subFunds(-1l);
     });
   }
 }
